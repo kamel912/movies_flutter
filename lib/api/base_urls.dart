e9ac1aceb2d56281ff73c3f4d4f3c75e9b3a46ce
Mohamed Kamel <mk.25390@gmail.com>
2021-02-04 12:32:36 +0200

    Update base_urls.dart

--- lib/api/base_urls.dart
+++ lib/api/base_urls.dart
@@ -4,7 +4,6 @@ final String moviesUrl = "{$baseUrl}/movie";
 final String smallPoster = "${postersUrlAuthority}w185";
 final String largePoster = "${postersUrlAuthority}w500";
 final String apiKey = "api_key";
-final String myApiKey = "308ca17c79e490f388403a0f237b056d";
 
 final String youtubeVideoBaseUrl = "https://www.youtube.com";
 final String youtubeImageBaseUrl = "https://img.youtube.com";
